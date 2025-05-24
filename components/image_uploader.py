b1c6274b34dace70f79bf52cc07ec3a601be37ec
Daffa Maulana <daffamaulana30@gmail.com>
2025-05-24 21:51:48 +0700

    Add image uploader component and update main application layout

---
+++ components/image_uploader.py
@@ -0,0 +1,123 @@
+import streamlit as st
+from PIL import Image
+import time
+import streamlit.components.v1 as components
+
+def deteksi_penyakit():
+    st.success("Fungsi deteksi dijalankan!")
+
+def upload_image():
+
+    # components.html(
+    #     """
+    #     <style>
+          
+    #     #fileInput {
+    #         display: none !important;
+    #     }
+
+    #     .labelInput {
+    #         display: flex;
+    #         flex-direction: column;
+    #         align-items: center;
+    #         width: 100%;
+    #         height: 500px !important;
+    #         background-color: #F4F4F4;
+    #         color: #008000;
+    #         text-align: center;
+    #         border-radius: 5px;
+    #         font-size: 80px;
+    #         font-weight: 600 !important;
+    #         cursor: pointer;
+    #     }
+    #     </style>
+    #     """,
+    #     height=100
+    # )
+    col1, col2 = st.columns([1, 1])
+    with col1:
+      uploaded_file = st.file_uploader("", label_visibility="collapsed", type=["jpg", "jpeg", "png"])
+    with col2:
+      if uploaded_file is not None:
+          image = Image.open(uploaded_file)
+
+          st.image(image, use_container_width=True)
+
+    # Tombol deteksi
+    st.markdown(
+    """
+    <script>
+      document.getElementByClass("eacrzsi2").value="New Button Text";
+    </script>
+
+    <style>
+    .st-emotion-cache-1gulkj5 {
+        display: flex;
+        flex-direction: column;
+        height: 300px;
+        justify-content: center !important;
+    }
+    div[data-testid="stFileUploaderDropzoneInstructions"] {
+        text-align: center !important;
+        justify-content: center !important;
+        width: 100% !important;
+        margin-bottom: 20px !important;
+    }
+    .st-emotion-cache-nwtri {
+        display: none !important;
+    }
+
+    .e16xj5sw5 {
+      margin-top: -65px;
+      margin-bottom: 12px;
+    }
+    
+    /* Define a class for your button */
+    button[kind="secondary"] {
+        width: 100% !important;
+    }
+    button[kind="primary"] {
+        background-color: #008000 !important;
+        color: white !important;
+        padding: 10px 20px !important;
+        border-radius: 5px !important;
+        border: 2px solid #008000 !important;
+        cursor: pointer !important;
+        transition: background-color 0.3s ease !important;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2) !important;
+    }
+    button[kind="primary"]:hover {
+        background-color: #fff !important;
+        color: #008000 !important;
+        border: 2px solid #008000 !important;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5) !important;
+    }
+    .detect-button {
+        background-color: #008000;
+        color: white;
+        margin: 20px;
+        padding: 10px 20px;
+        border-radius: 5px;
+        border: 2px solid #008000;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    }
+
+    /* Define the hover state for the button */
+    .detect-button:hover {
+        background-color: #fff;
+        color: #008000;
+        border: 2px solid #008000;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+    
+    left, middle, right = st.columns(3)
+    if middle.button('Deteksi Penyakit', type="primary", use_container_width=True):
+      deteksi_penyakit()
+
+    # return uploaded_file if detect and uploaded_file is not None else None
--- main.py
+++ main.py
@@ -1,6 +1,54 @@
 import streamlit as st
+from PIL import Image
+from components.image_uploader import upload_image
 
-st.set_page_config(page_title="Dashboard", page_icon="🧠")
+# --- Page Configuration ---
+st.set_page_config(
+    page_title="CabAI",
+    page_icon="https://raw.githubusercontent.com/daffamaul30/CabAI/main/assets/icon.png", # Replace with your CabAI favicon URL
+    layout="centered"
+)
 
-st.title("Beranda Aplikasi Streamlit")
-st.write("Selamat datang di aplikasi Streamlit kamu 🎉")
+st.markdown(
+    """
+<style>
+.stAppHeader {
+    display: none;
+}
+.block-container {
+    padding-top: 0rem;
+    padding-bottom: 0rem;
+    padding-left: 0rem;
+    padding-right: 0rem;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+# --- Header ---
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("https://raw.githubusercontent.com/daffamaul30/CabAI/main/assets/logo.png", width=300)
+
+
+st.markdown("#### Deteksi Penyakit Tanaman Cabai")
+image = upload_image()
+
+if image:
+    st.success("Gambar berhasil diunggah!")
+
+st.markdown("<br><br>", unsafe_allow_html=True)
+st.markdown("#### Perkiraan Kebutuhan Pasar")
+
+st.markdown("<br>", unsafe_allow_html=True)
+chart_data = {
+    "Month": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    "Value": [500, 350, 550, 400, 500, 450, 380, 580, 380, 450, 500, 550]
+}
+
+# Create a simple line chart
+st.line_chart(chart_data, x="Month", y="Value")
+
+
+# --- Chatbox (Placeholder) ---
